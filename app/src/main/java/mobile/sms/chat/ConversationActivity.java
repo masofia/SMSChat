d0e91caadab02d2cfba95dd2f995fd256b43db8e
masofia <sofia@Marias-MBP.lan1>
2019-11-14 00:54:06 -0500

    cleanup

--- app/src/main/java/mobile/sms/chat/ConversationActivity.java
+++ app/src/main/java/mobile/sms/chat/ConversationActivity.java
@@ -81,33 +81,6 @@ public class ConversationActivity extends AppCompatActivity {
     }
 
     public void sendMessage(String mssg, String contact) {
-        // test long
-        mssg = "# Chat App\n" +
-                "\n" +
-                "## Open Items / Improvements:\n" +
-                "\n" +
-                "- SMS messages sent and received showing up in other chat? Is it possible to mute it from the other app?\n" +
-                "If not it should be fine since the messages displayed there will be encrypted anyway. \n" +
-                "\n" +
-                "\"If the user has multiple SMS messaging apps installed on the Android phone, the App chooser will appear with a list of these apps, and the user can choose which one to use. (Android smartphones will have at least one, such as Messenger.)\"\n" +
-                "(https://google-developer-training.github.io/android-developer-phone-sms-course/Lesson%202/2_p_sending_sms_messages.html)\n" +
-                "\n" +
-                "\"You can manage SMS operations such as dividing a message into fragments, sending a multipart message, get carrier-dependent configuration values, and so on.\" -- will need to break up encrypted messages as they may be too long\n" +
-                "(https://google-developer-training.github.io/android-developer-phone-sms-course/Lesson%202/2_p_sending_sms_messages.html)\n" +
-                "\n" +
-                "- The App does not use any DB. When the app is closed, all message history is lost. A ConversationActivity contains message history, but when you exit the specific activity (and go back to main), it gets lost. For a message to be received, one must be on the ConversationActivity screen. \n" +
-                "\n" +
-                "- Use shared ViewModel? Use fragments?\n" +
-                "https://stackoverflow.com/questions/44641121/share-viewmodel-between-fragments-that-are-in-different-activity\n" +
-                "https://stackoverflow.com/questions/27466397/trying-create-a-chat-with-fragment\n" +
-                "https://blog.mindorks.com/shared-viewmodel-in-android-shared-between-fragments\n" +
-                "\n" +
-                "\n" +
-                "## Resources:\n" +
-                "\n" +
-                "- https://google-developer-training.github.io/android-developer-phone-sms-course\n" +
-                "- https://medium.com/@anitaa_1990/how-to-update-an-activity-from-background-service-or-a-broadcastreceiver-6dabdb5cef74 \n" +
-                "- https://medium.com/@peterekeneeze/passing-data-between-activities-2d0ef122f19d";
         try {
             SmsManager smgr = SmsManager.getDefault();
 
